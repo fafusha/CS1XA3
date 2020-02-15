17e0cf3189ede4ff8138560aca087a0cf2c98c31
Dennis Rapoport <dennis.rapoport@gmail.com>
2020-02-15 03:53:30 +0000

    daily update

--- Project01/project_analyze.sh
+++ Project01/project_analyze.sh
@@ -1,76 +1,72 @@
 #!/bin/bash
 
-#TODO
-#IMPT
-# A)
-# - Account for directories/files including special characters
-#   (i.e. whitespace)
-# 
-# B)
-# - Account for directories/files not existing/already existing 
-#
-# C)
-# - Account for command IO failure
-
-
-# Undocumented features
-# 
-# Ability to change directorires, make wdir and odir
-# Make the set by last argument account for odir and wdir separetly
-
-
-# TODO
-# Feature Ideas
-# 
-# Do standart output thingies...
-
-#Variables
-#DDIR: default directory
-#WDIR: working directory
-#ODIR: output directory
-
-#FIXME
-#
-# Fix directories, make it an argument, default to .
-# Works temporarirle
-#
-
-
-# Think about directories more, possibly an error here
-DDIR='/home/rapopord/private/CS1XA3/Project01/'
-WDIR="$DDIR"
-ODIR="$DDIR"
-#cd "$WDIR"
-
-# make take flags with arguments
-#
-# consider using swithc case staements
+REPO_DIR="$(git rev-parse --show-toplevel)"
+OUT_DIR="$REPO_DIR/Project01/"
+# Feature 6.1
+FIX_ME=0
+FILE_SIZE_LIST=0
+FILE_TYPE_COUNT=0
+FILES=()
 for arg in "$@"
 do
 	case $arg in
 		-fm|--fixme)
-			# FIXME
-			# Fix issue with directories
-			# Make custom directory io with |
-			grep -l -R '.*#FIXME.*$'  > "fixme.log"
+			FIX_ME=1
 			shift
-		;;
+			;;
 		-fsl|--file-size-list)
-			#FIXME
-			# Fix issues with directories
-			# Makke custom directory io
-			ls -S -s -h | more
+			FILE_SIZE_LIST=1
 			shift
-		;;
-		-ftc|--file-type-count)
-			#FIXME
-			#
-			#Do nice output
-			#
-			# Fix issue with directories
-			read -p $'Enter extension\n' fileext
-			ls -r | grep ".*\.$fileext\b" | wc -l
+			;;
+		-ftc|--file-type-coun)
+			FILE_TYPE_COUNT=1
 			shift
-		;;
+			;;
+		-*|--*)
+			echo "$0: unrecognized option:  '$1'"
+			echo "Check README.md for help."
+			exit 1
+			;;
+		*)
+			if [ -e $1 ] ; then
+				FILES+=("$1")
+				shift
+			else
+				echo "$0: cannot access: '$1'  No such file or directory"
+				echo "Check README.md for help."
+				exit 1
+			fi
+			;;
 	esac
 done
+
+# Checking if any FILES were passed as args
+if [ ${#FILES[@]} -eq 0 ] ; then
+	cd "$REPO_DIR"
+	FILES+=(".")
+fi
+
+WDIR="${FILES[@]}"
+
+# Feature 6.2
+if [ "$FIX_ME" -eq 1 ] ; then
+       	grep -l -r '.*#FIXME.*$' "$WDIR" > "$OUT_DIR/fixme.log"
+fi
+
+# Feature 6.4
+if [ "$FILE_SIZE_LIST" -eq 1 ] ; then
+	ls -R -S -G -s -h "$WDIR" | more	
+fi
+
+# Feature 6.5
+if [ "$FILE_TYPE_COUNT" -eq 1 ] ; then
+	read -p "$0: Enter extension: " FTYPE
+	if [[ $FTYPE == *[[:space:]]* ]]
+	then
+		echo "$0: Inavlid extension: '$FTYPE'"
+	       	echo "$0: Check README.md for help."
+		exit 1
+	fi
+	echo "Files with extension $FTYPE:"
+	ls -r "$WDIR"| grep ".*\.$FTYPE$" | wc -l 
+fi
